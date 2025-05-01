a5e5d55f68a9a1949f34b2de871b3ebbd4ff7d37
alireza <alirezarazani3185@gmail.com>
2025-05-01 21:00:34 +0330

    add .env conf

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ from dotenv import load_dotenv
 from collections import defaultdict
 import re
 
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+USER_STATE = {}
+USER_LAST_REQUEST = defaultdict(lambda: 0)
+ALLOWED_USERS = os.getenv('ALLOWED_USER')
+
 
