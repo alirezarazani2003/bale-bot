4b8145d9bcb43bc366bd8c114117b608e0e3c79d
alireza <alirezarazani3185@gmail.com>
2025-05-01 21:03:49 +0330

    load or save channels for each user based on chat_id

--- main.py
+++ main.py
@@ -12,4 +12,15 @@ USER_STATE = {}
 USER_LAST_REQUEST = defaultdict(lambda: 0)
 ALLOWED_USERS = os.getenv('ALLOWED_USER')
 
-
+def load_channels(chat_id):
+    user_channels_file = f'{chat_id}_channels.json'
+    if os.path.exists(user_channels_file):
+        with open(user_channels_file, 'r') as f:
+            return json.load(f)
+    else:
+        return []
+    
+def save_channels(chat_id, channels):
+    user_channels_file = f'{chat_id}_channels.json'
+    with open(user_channels_file, 'w') as f:
+        json.dump(channels, f)
